bdfb4eab2a92d3f2f296d83f1c5cd1d96fdb1095
colttylerwilson <colt.wilson@sjsu.edu>
2017-08-03 20:49:46 -0700

    Fixed error using 0 as an input

--- HammingDistance.js
+++ HammingDistance.js
@@ -4,39 +4,51 @@
  * @return {number} HD
  */
 var hammingdistance = function (x, y) {
-    var xB = x.toString(2);
-    var yB = y.toString(2);
+    this.x = x;
+    this.y = y;
+    var xB = this.x.toString(2);
+    var yB = this.y.toString(2);
+
     var countX = 0;
     var countY = 0;
-    while (x > 0) {
-        x = Math.floor(x / 2);
-        countX = countX + 1;
+
+    if (x == 0) {
+        countX = 1;
     }
-    while (y > 0) {
-        y = Math.floor(y / 2);
-        countY = countY + 1;
+    else {
+        while (x > 0) {
+            x = Math.floor(x / 2);
+            countX = ++countX;
+        }
     }
+    if (y == 0) {
+        countY = 1;
+    }
+    else {
+        while (y > 0) {
+            y = Math.floor(y / 2);
+            countY = ++countY;
+        }
+    }
+    
     if (countX > countY) {
         for (var i = 0; i < Math.abs(countY - countX); i++) {
             yB = "0" + yB;
         }
     }
-    else
-    {
+    else {
         for (var i = 0; i < Math.abs(countY - countX); i++) {
             xB = "0" + xB;
         }
     }
 
     var HD = 0;
-    for(var i = 0; i < yB.length; i++)
-    {
-        if(yB.charAt(i) != (xB.charAt(i)))
-        {   
+    for (var i = 0; i < yB.length; i++) {
+        if (yB.charAt(i) != (xB.charAt(i))) {
             HD = HD + 1;
         }
     }
     return HD;
 };
 
-console.log(hammingdistance(4, 2));
+console.log(hammingdistance(2147483647, 0));
