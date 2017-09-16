bf3a2aa0be1badd3176d3c612005a8377a79006b
Colt Wilson <colt.t.wilson@gmail.com>
2017-09-16 08:58:58 -0700

    Commit All

--- SingleNumber/SingleNumber Solution.js
+++ SingleNumber/SingleNumber Solution.js
@@ -3,15 +3,11 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    //Container for the result
-        var result = 0;
-        for(var i = 0 ; i < nums.length; i++)
-        {
-            //XOR all elements in the array
-            //This works becuase if you have two of the same numbers,
-            // then XORing them together will give you a balance of ZERO
-            //What remains after XORing all elements in the array is the single number
-            result = result ^ nums[i];
-        }
-        return result;
+    var result = 0;
+
+    for(var i = 0 ; i < nums.length; i++)
+    {
+        result ^= nums[i];
+    }
+    return result;
 };
